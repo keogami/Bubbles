def04b68d58d6259683ccf902b2a96ac6f472829
keogami <41939011+keogami@users.noreply.github.com>
2022-02-12 00:24:59 +0530

    bubbles now react to the mouse movement

--- components/Bubble.js
+++ components/Bubble.js
@@ -4,15 +4,20 @@ class Bubble {
     coord = {x: 50, y: 50},
     velocity = .125,
     size = 4,
+    sway = 0.05,
   } = {}) {
     this._color = color
     this._coord = coord
     this._velocity = velocity
     this._size = size
+    this._sway = sway
   }
 
-  update(timestamp) {
+  update({timestamp, displacement}) {
     this._coord.y -= this._velocity
+
+    this._coord.x += (displacement.x * this._sway)
+    this._coord.y += (displacement.y * this._sway)
   }
 
   draw({ctx}) {
