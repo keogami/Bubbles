6eedc41d9c30afae5a2b5be81ebdb931abfc991b
keogami <41939011+keogami@users.noreply.github.com>
2022-02-11 20:58:42 +0530

    bubbles are now spawned and deleted from the memory when they go out of the screen

--- components/Bubbles.js
+++ components/Bubbles.js
@@ -1,6 +1,12 @@
 import "./Bubble.js"
 import Bubble from "./Bubble.js"
 
+const isInRange = (min, max) => (n) => (n >= min) && (n <= max)
+const isInBound = (inRangeX, inRangeY) => ({x, y}) => inRangeX(x) && inRangeY(y)
+const isInRect = (x, y, width, height) => (
+  isInBound(isInRange(x, x + width), isInRange(y, y + height))
+)
+
 class Bubbles {
   constructor(canvas, {color="black", size=3, velocity=2} = {}) {
     this.canvas = canvas
@@ -12,6 +18,7 @@ class Bubbles {
     this._velocity = velocity
 
     this._spawnBubbleTimeout = null
+    this._isInScreen = isInRect(0, 0, this.canvas.width, this.canvas.height)
   }
 
   color(c) {
@@ -32,6 +39,9 @@ class Bubbles {
   update(timestamp) {
     for (let bubble of this.bubbleSet) {
       bubble.update(timestamp)
+      if (!this._isInScreen(bubble._coord)) {
+        this.bubbleSet.delete(bubble)
+      }
     }
   }
 
@@ -42,13 +52,20 @@ class Bubbles {
     }
   }
 
+  startSpawningBubbles() {
+    this._spawnBubbleTimeout = setTimeout((function spawnBubble() {
+      this.bubbleSet.add(new Bubble({
+        color: this._color,
+        coord: {x: this.canvas.width/2, y: this.canvas.height/2},
+        velocity: this._velocity,
+        size: this._size
+      }))
+      this._spawnBubbleTimeout = setTimeout(spawnBubble.bind(this), 1000)
+    }).bind(this), 1000)
+  }
+
   run() {
-    this.bubbleSet.add(new Bubble({
-      color: this._color,
-      coord: {x: this.canvas.width/2, y: this.canvas.height/2},
-      velocity: this._velocity,
-      size: this._size
-    }))
+    this.startSpawningBubbles()
     const frame = (timestamp) => {
       requestAnimationFrame(frame)
 
