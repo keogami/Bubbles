b93934f212cebc852ed721e19831d2386b5c926a
keogami <41939011+keogami@users.noreply.github.com>
2022-02-11 23:44:20 +0530

    added bubble variants

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 new Bubbles(canvas, [
-  {
-    color: "black", size: 4, velocity: 0.325,
-  }
+  { color: "black", size: 5, velocity: 0.550, }, // lighter
+  { color: "black", size: 4, velocity: 0.325, }, // normal
+  { color: "black", size: 3, velocity: 0.225, }, // heavier
 ]).run()
